31c6a5175c4af9c2ddd5a60535a8fa12e0ab767e
Kaustubh <kaustubhsingh37@gmail.com>
2024-03-03 03:50:30 +0530

    no changes faltu commit for checking

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="h-full w-full">
+      
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
