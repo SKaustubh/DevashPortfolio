aab97a6f35573e68a1d0c778131500f6484a7d44
Kaustubh <kaustubhsingh37@gmail.com>
2024-03-11 03:26:50 +0530

    done projects phot added

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  RxDiscordLogo,
   RxGithubLogo,
   RxInstagramLogo,
   RxTwitterLogo,
@@ -22,7 +21,9 @@ const Footer = () => {
               className="flex flex-row items-center my-[15px] cursor-pointer"
             >
               <FaYoutube />
-              <span className="text-[15px] ml-[6px]">Youtube</span>
+              <span className="text-[15px] ml-[6px] cursor-pointer">
+                Youtube
+              </span>
             </a>
             <a
               href="https://github.com/SKaustubh"
@@ -31,7 +32,9 @@ const Footer = () => {
               className="flex flex-row items-center my-[15px] cursor-pointer"
             >
               <RxGithubLogo />
-              <span className="text-[15px] ml-[6px]">Github</span>
+              <span className="text-[15px] ml-[6px] cursor-pointer">
+                Github
+              </span>
             </a>
           </div>
           <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
@@ -43,16 +46,20 @@ const Footer = () => {
               className="flex flex-row items-center my-[15px] cursor-pointer"
             >
               <RxInstagramLogo />
-              <span className="text-[15px] ml-[6px]">Instagram</span>
+              <span className="text-[15px] ml-[6px] cursor-pointer">
+                Instagram
+              </span>
             </a>
             <a
               href="https://twitter.com/Singhkaustubh3"
               target="_blank"
               rel="noopener noreferrer"
-              className="flex flex-row items-center my-[15px] cursor-pointer"
+              className="flex flex-row items-center my-[15px] cursor-pointer cursor-pointer"
             >
               <RxTwitterLogo />
-              <span className="text-[15px] ml-[6px]">Twitter</span>
+              <span className="text-[15px] ml-[6px] cursor-pointer">
+                Twitter
+              </span>
             </a>
             <a
               href="https://www.linkedin.com/in/kaustubh-singh-11-gt/"
@@ -61,19 +68,30 @@ const Footer = () => {
               className="flex flex-row items-center my-[15px] cursor-pointer"
             >
               <RxLinkedinLogo />
-              <span className="text-[15px] ml-[6px]">Linkedin</span>
+              <span className="text-[15px] ml-[6px] cursor-pointer">
+                Linkedin
+              </span>
             </a>
           </div>
           <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
             <div className="font-bold text-[16px]">About</div>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <a href="#" className="text-[15px] ml-[6px]">Become Sponsor</a>
+              <a href="#" className="text-[15px] ml-[6px]">
+                Become Sponsor
+              </a>
             </p>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <a href="#" className="text-[15px] ml-[6px]">Learning about me</a>
+              <a href="#" className="text-[15px] ml-[6px]">
+                Learning about me
+              </a>
             </p>
             <p className="flex flex-row items-center my-[15px] cursor-pointer">
-              <a href="mailto:kaustubhsingh37@gmail.com" className="text-[15px] ml-[6px]">kaustubhsingh37@gmail.com</a>
+              <a
+                href="mailto:kaustubhsingh37@gmail.com"
+                className="text-[15px] ml-[6px]"
+              >
+                kaustubhsingh37@gmail.com
+              </a>
             </p>
           </div>
         </div>
--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -18,13 +18,13 @@ const Projects = () => {
           A modern Next.js portfolio is a polished and efficient showcase of a developer's expertise, projects, and accomplishments, crafted using the Next.js framework for its powerful capabilities in server-side rendering, static site generation, and seamless client-side navigation. Featuring a clean and responsive design, the portfolio integrates CSS frameworks like Tailwind CSS or styled-components for aesthetic appeal and customization. Leveraging APIs and databases enables dynamic content presentation, while Next.js's built-in support for SEO optimization and performance enhancements ensures a smooth and engaging user experience, making the portfolio an effective tool for attracting potential clients or employers."
         />
         <ProjectCard
-          src="/CardImage.png"
+          src="/blogapp.jpg"
           title="Blog App"
           description="
           The Blog App is a dynamic and user-friendly platform built with React.js as the front-end framework and Appwrite as the backend service. This innovative combination allows users to create, edit, and publish blog posts effortlessly while enjoying a seamless and responsive user experience. Leveraging the power of React.js, the app offers an intuitive interface with features such as rich text editing, real-time previews, and interactive components for a visually engaging blog creation process. Meanwhile, with Appwrite handling the backend operations, including user authentication, database management, and file storage, the Blog App ensures robust security and scalability, enabling users to focus on crafting captivating content without worrying about technical complexities. Whether you're a seasoned blogger or a newcomer to the world of content creation, the Blog App powered by React.js and Appwrite offers an unparalleled platform to express your ideas and connect with your audience effortlessly."
         />
         <ProjectCard
-          src=""
+          src="/diet.jpg"
           title="Diet Tracker"
           description="
           A diet tracker application built using the MERN (MongoDB, Express.js, React, Node.js) stack and styled with Bootstrap CSS offers users a seamless and intuitive platform to monitor and manage their dietary habits effectively. With MongoDB serving as the database to store user data, Express.js providing a robust backend framework, and Node.js powering the server, the application ensures reliable data storage and retrieval. Utilizing React on the frontend enables dynamic and interactive user interfaces, allowing users to easily track their meals, calorie intake, and nutritional information. Bootstrap CSS enhances the application's visual appeal and responsiveness, providing a clean and modern design that adapts seamlessly across various devices. Users can log their meals, set dietary goals, and view detailed reports and analytics to make informed decisions about their nutrition and health. With its user-friendly interface and comprehensive features, the diet tracker application empowers users to achieve their wellness goals and maintain a healthy lifestyle effortlessly."
